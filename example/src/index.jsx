0c7c44d52a583ee6f4444f0e917c72d421337f7a
krazijames <krazijames@ridi.com>
2018-03-16 11:10:23 +0900

    Remove react/jsx-filename-extension lint rule

--- example/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Menu } from '@ridi/cms-ui';
-
-ReactDOM.render(<Menu />, document.getElementById('root'));
-
-if (module.hot) {
-  module.hot.accept();
-}
