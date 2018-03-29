5ee366170dd4162e70c8fd4e5a84814e186af16d
krazijames <krazijames@ridi.com>
2018-03-29 16:52:34 +0900

    Move components to components directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import * as Components from '.';
 
-export { default as Menu } from './Menu';
+export { default as Menu } from './components/Menu';
 
 export default Components;
