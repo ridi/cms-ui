c3ba6adee2093fb590951c766b7bb9caf62ea7fb
krazijames <krazijames@ridi.com>
2018-04-16 19:36:15 +0900

    Reduce output size (#17)
    * Explicitly import Font Awesome icons
    
    * Selectively build lodash
    
    * Selectively build bootstrap

--- src/components/FontAwesome.js
+++ src/components/FontAwesome.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import fontawesome from '@fortawesome/fontawesome';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import * as fa from '@fortawesome/fontawesome-free-solid';
-
-fontawesome.library.add(...fa);
 
 export default function FontAwesome(props) {
   return <FontAwesomeIcon fixedWidth {...props} />;
