3ce4e49821b2a992c3f2d6aab777ca2626bc517c
krazijames <krazijames@ridi.com>
2018-04-18 17:12:58 +0900

    Add ListMenu CSS

--- src/components/Menu/ListMenu/index.js
+++ src/components/Menu/ListMenu/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { Nav } from 'reactstrap';
-import { getRestProps } from '../../utils/component';
-import MenuItem from './MenuItem';
+import { getRestProps } from '../../../utils/component';
+import { modularizeClassNames as cm } from '../../../utils/css';
+import MenuItem from '../MenuItem';
 
 export default class ListMenu extends React.PureComponent {
   static propTypes = {
@@ -17,7 +18,7 @@ export default class ListMenu extends React.PureComponent {
   render() {
     const { items } = this.props;
     return (
-      <Nav vertical {...getRestProps(this)}>
+      <Nav className={cm('list_menu')} vertical {...getRestProps(this)}>
         {_.map(items, (item, key) => (
           <MenuItem key={key} item={item} />
         ))}
