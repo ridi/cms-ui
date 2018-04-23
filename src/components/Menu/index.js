55d246a92d085f95f4ebba904ea80f5a75950a7a
krazijames <krazijames@ridi.com>
2018-04-23 18:13:56 +0900

    Explicitly define prop types

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,27 @@
-import { compose, mapProps } from 'recompose';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { modularizeParentCss } from '../../higherOrderComponents';
 import propsMapper from './propsMapper';
 import CompositeMenu from './CompositeMenu';
 
-export default compose(
-  modularizeParentCss(),
-  mapProps(propsMapper),
-)(CompositeMenu);
+class Menu extends React.PureComponent {
+  static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      menu_title: PropTypes.string,
+      menu_url: PropTypes.string,
+      is_newtab: PropTypes.bool,
+      menu_deep: PropTypes.number,
+    })),
+  };
+
+  static defaultProps = {
+    items: undefined,
+  };
+
+  render() {
+    return <CompositeMenu {...propsMapper(this.props)} />;
+  }
+}
+
+export default modularizeParentCss()(Menu);
