a6df0ff9630c6df50efbb9e5251b4129cadbbf3e
krazijames <krazijames@ridi.com>
2018-04-24 12:04:33 +0900

    Set HOC's display name

--- src/higherOrderComponents/wrapWithCssModule.js
+++ src/higherOrderComponents/wrapWithCssModule.js
@@ -3,15 +3,26 @@ react/no-find-dom-node: 0
 */
 
 import ReactDOM from 'react-dom';
-import { lifeCycle } from 'recompose';
+import { lifeCycle, setDisplayName, wrapDisplayName } from 'recompose';
 import { getGlobalCssModule } from '../utils/css';
 
 export default function wrapWithCssModule() {
-  return lifeCycle({
-    componentDidMount() {
-      const { parentNode } = ReactDOM.findDOMNode(this);
-      const rootClassName = getGlobalCssModule().root;
-      parentNode.classList.add(rootClassName);
-    },
-  });
+  return (Component) => {
+    const enhance = lifeCycle({
+      componentDidMount() {
+        const { parentNode } = ReactDOM.findDOMNode(this);
+        const rootClassName = getGlobalCssModule().root;
+        parentNode.classList.add(rootClassName);
+      },
+    });
+
+    const WrappedComponent = enhance(Component);
+
+    if (process.env.NODE_ENV !== 'production') {
+      const wrappedName = wrapDisplayName(Component, 'wrapWithCssModule');
+      return setDisplayName(wrappedName)(WrappedComponent);
+    }
+
+    return WrappedComponent;
+  };
 }
