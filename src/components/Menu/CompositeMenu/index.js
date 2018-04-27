de0bbb3e0f5c9f14009baa0b9617da6258592247
krazijames <krazijames@ridi.com>
2018-04-27 12:24:36 +0900

    Lock screen scroll on open menu

--- src/components/Menu/CompositeMenu/index.js
+++ src/components/Menu/CompositeMenu/index.js
@@ -3,22 +3,30 @@ import PropTypes from 'prop-types';
 import es6ClassBindAll from 'es6-class-bind-all';
 import { ButtonGroup, Collapse, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
 import { getPassThroughProps } from '../../../utils/component';
-import { modularizeClassNames as cm } from '../../../utils/css';
+import { lockRootScroll, modularizeClassNames as cm } from '../../../utils/css';
 import MeButton from '../../buttons/MeButton';
 import LogoutButton from '../../buttons/LogoutButton';
 import FilterableMenu from '../FilterableMenu';
+
+const mediaQueryList = window.matchMedia('(min-width: 1200px)');
+
+function unlockRootScroll(e) {
+  if (!e.matches) {
+    return;
+  }
+
+  lockRootScroll(false);
+}
 
 export default class CompositeMenu extends React.Component {
   static propTypes = {
     className: PropTypes.string,
     items: FilterableMenu.propTypes.items,
-    collapse: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
   };
 
   static defaultProps = {
     className: undefined,
     items: FilterableMenu.defaultProps.items,
-    collapse: 'xl',
   };
 
   constructor(props) {
@@ -31,25 +39,29 @@ export default class CompositeMenu extends React.Component {
     };
   }
 
+  componentDidMount() {
+    mediaQueryList.addListener(unlockRootScroll);
+  }
+
+  componentWillUnmount() {
+    mediaQueryList.removeListener(unlockRootScroll);
+  }
+
   toggle() {
+    const isOpen = !this.state.isOpen;
     this.setState({
-      isOpen: !this.state.isOpen,
+      isOpen,
     });
+    lockRootScroll(isOpen);
   }
 
   render() {
-    const { className, items, collapse } = this.props;
+    const { className, items } = this.props;
     const { isOpen } = this.state;
 
     return (
-      <div
-        className={cm(
-          'composite_menu',
-          className,
-        )}
-        {...getPassThroughProps(this)}
-      >
-        <Navbar className={cm('navigation_bar')} expand={collapse} color="primary" dark>
+      <div className={cm('composite_menu', className)} {...getPassThroughProps(this)}>
+        <Navbar className={cm('navigation_bar')} expand="xl" color="primary" dark>
           <NavbarBrand className={cm('title')} tag="h1">
             <a href="/">Ridibooks CMS</a>
           </NavbarBrand>
@@ -57,7 +69,7 @@ export default class CompositeMenu extends React.Component {
           <NavbarToggler onClick={this.toggle} />
         </Navbar>
 
-        <Collapse className={cm('content', `d-${collapse}-flex`)} isOpen={isOpen}>
+        <Collapse className={cm('content', 'd-xl-flex')} isOpen={isOpen}>
           <ButtonGroup className={cm('button_container')} size="sm">
             <MeButton tag="a" color="link" />
             <LogoutButton tag="a" color="link" />
