dfbcacfc04ee7eebdd42b3ae5a1c79b6eec11e51
krazijames <krazijames@ridi.com>
2018-05-03 18:12:41 +0900

    Wrap root component with extra DOM

--- src/higherOrderComponents/wrapWithCssModule.js
+++ src/higherOrderComponents/wrapWithCssModule.js
@@ -2,24 +2,19 @@
 react/no-find-dom-node: 0
 */
 
-import ReactDOM from 'react-dom';
-import { compose, lifeCycle, setDisplayName, setPropTypes, wrapDisplayName } from 'recompose';
+import React from 'react';
+import { setDisplayName, wrapDisplayName } from 'recompose';
 import { getGlobalCssModule } from '../utils/css';
 
 export default function wrapWithCssModule() {
   return (Component) => {
-    const enhance = compose(
-      setPropTypes(Component.propTypes),
-      lifeCycle({
-        componentDidMount() {
-          const { parentNode } = ReactDOM.findDOMNode(this);
-          const rootClassName = getGlobalCssModule().root;
-          parentNode.classList.add(rootClassName);
-        },
-      }),
+    const WrappedComponent = props => (
+      <div className={getGlobalCssModule().root}>
+        <Component {...props} />
+      </div>
     );
 
-    const WrappedComponent = enhance(Component);
+    WrappedComponent.propTypes = Component.propTypes;
 
     if (process.env.NODE_ENV !== 'production') {
       const wrappedName = wrapDisplayName(Component, 'wrapWithCssModule');
