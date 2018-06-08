cf7bdf6e57c03570077a83dcb3976b552988cdf7
krazijames <krazijames@ridi.com>
2018-06-08 17:55:06 +0900

    Export library version

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cssModule from './styles/index.module.scss';
 
 setGlobalCssModule(cssModule, 'cms_ui');
 
+export { version } from '../package.json';
+
 export * from './utils/render';
 
 export { default as Menu } from './components/Menu';
